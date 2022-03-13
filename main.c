125ec61b4ebd643d9f60d4114e589593c93b0538
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-03-13 17:30:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,19 +10,25 @@ void pantalla_final(char m[3][3]);
 
 int main()
 {
-     char m[3][3];
-     void loop(m);
-     return 0;
+      char m[3][3];
+      for(int i = 0;i<3;i++)
+      {
+           for(int j = 0;j<3;j++){
+                m[i][j];
+           }
+      }
+      void loop(m);
+      return 0;
 };
 
 
 void loop(char m[3][3])
 {
-     void mostrar_tablero(m);
-     void turno(m);
-     void colocar_pieza(m);
-     void victoria(m);
-     void pantalla_final(m);
+      void mostrar_tablero(m);
+      void turno(m);
+      void colocar_pieza(m);
+      void victoria(m);
+      void pantalla_final(m);
 };
 
 
