24cbd8c70fdccce9b535a1e08b248414ce9e66c9
miguelooon <101124840+miguelooon@users.noreply.github.com>
2022-03-14 20:11:36 -0300

    Update main.c
    Marcar casilla

--- main.c
+++ main.c
@@ -48,7 +48,115 @@ void turno(char m[3][3])
 
 void colocar_pieza(char m[3][3])
 {
+    int aux;
+    int i,j,r;
 
+    do{
+       
+        do{
+        printf("Elige una casilla basandote en los numeros del tablero:\n");
+        fflush(stdin);
+        scanf("%i",&aux);
+        } while (aux<1||aux>9);
+
+        r=0;
+
+       
+        switch(aux){
+                  
+            case 1:{
+                i=0;
+                j=0;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.\n\n");
+                }
+                break;
+            }
+                  
+            case 2:{
+                i=0;
+                j=1;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 3:{
+                i=0;
+                j=2;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 4:{
+                i=1;
+                j=0;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 5:{
+                i=1;
+                j=1;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 6:{
+                i=1;
+                j=2;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 7:{
+                i=2;
+                j=0;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 8:{
+                i=2;
+                j=1;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+                  
+            case 9:{
+                i=2;
+                j=2;
+                if (m[i][j]=='X'|| m[i][j]=='O'){
+                    r=1;
+                    printf("La casilla no esta disponible, selecciona una que este disponible.");
+                }
+                break;
+            }
+        }
+    }while (r==1);
+    m[i][j] = 'X';   
+        
 };
 
 
